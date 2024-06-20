e6dfba732e9a7c1831f1ae8c01c42e2e939017a7
Glitch752 <Glitch752>
2024-06-19 20:24:20 -0500

    Argument parsing and file opening

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,54 @@
 const std = @import("std");
 
 pub fn main() !void {
+    // A general purpose allocator -- needed for nearly everything requiring memory allocation
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+    // Get the standard output file
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // Print the process arguments
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    try bw.flush(); // don't forget to flush!
+    // // Temporary: print the arguments to the program
+    // for (args) |arg| {
+    //     try stdout.print("{s} ", .{arg});
+    // }
+
+    if (args.len < 2) {
+        try stdout.print("Usage: {s} <file>\n", .{ .name_string = args[0] });
+        try bw.flush();
+        return;
+    }
+
+    // Open the file in the arguments of the program
+    const file_path = args[1];
+    const file = try std.fs.cwd().openFile(file_path, .{ .mode = .read_only }) catch |err|
+        switch (err) {
+        .fileNotFound => {
+            try stdout.print("File not found: {s}\n", .{ .name_string = file_path });
+            return;
+        },
+        else => return err,
+    };
+
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    // Read the file and print it to stdout
+    var line = try in_stream.readLine(allocator);
+
+    while (line != null) {
+        try stdout.print("{s}\n", .{line});
+        line = try in_stream.readLine(allocator);
+    }
+
+    try bw.flush();
 }
